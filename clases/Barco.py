e4ac8139e64e6ed5c4e437763ba4a8268524908f
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-16 19:36:10 +0100

    corrigiendo cambios

--- clases/Barco.py
+++ clases/Barco.py
@@ -1,7 +1,7 @@
 import sys
 
 from itertools import cycle, chain, product, repeat
-from clases import Case
+from clases.Case import Case1
 from clases import Conventions
 from random import shuffle, choice, random
 
@@ -9,11 +9,12 @@ HORIZONTAL = 0
 VERTICAL = 1
 
 ORIENTACIONES = (VERTICAL, HORIZONTAL)
-instances = []
-casillas_ocupadas = set()
+
 
 
 class Barco1:
+    instances = []
+    casillas_ocupadas = set()
     
     def __init__(self, longitud):
             self.longitud = longitud
@@ -33,7 +34,7 @@ class Barco1:
                     primero = choice(range(num_columnas + 1 - longitud)) #choice
                     letra = num2l(rang)
                     cifras = [num2c(x) for x in range(primero, primero + longitud)]
-                    self.casillas = {Case.instances[l + c]
+                    self.casillas = {Case1.instances[l + c] #Case
                                 for l, c in product(repeat(letra, longitud), cifras)}
                 else:
                     rang = choice(range(num_columnas)) #choice
@@ -41,20 +42,20 @@ class Barco1:
                     cifra = num2c(rang)
                     letras = [num2l(x) for x in range(primero, primero + longitud)]
                     # Crear el barco
-                    self.casillas = {Case.instances[l + c]
+                    self.casillas = {Case1.instances[l + c] #Case
                                 for l, c in product(letras, repeat(cifra, longitud))}
 
-                for existente in instances: #Barco.instances
+                for existente in Barco1.instances: #Barco.instances
                     if self.casillas.intersection(existente.casillas):
                         break  # break relativo al "for existente in barcos:"
                 else:
                     # Agregar el barco en el contenedor de barcos
-                    instances.append(self) #Barco.instances.append(self)
+                    Barco1.instances.append(self) #Barco.instances.append(self)
                     # Informar la casilla que contiene un barco.
                     for casilla in self.casillas:
                         casilla.barco = self
                     # Agregar estas casillas a las casillas ocupadas :
-                    casillas_ocupadas |= self.casillas #Barco.casillas_ocupadas |= self.casillas
+                    Barco1.casillas_ocupadas |= self.casillas #Barco.casillas_ocupadas |= self.casillas
                     break  # break relativo al "while True:"
 
     @classmethod
